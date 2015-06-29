769cf90434fa2e84ae175627b2b0dba7b5e9722d
D3strukt0rLP <d3strukt0rlp@hotmail.com>
2015-06-29 17:46:01 +0200

    Create agario-hack.js

---
+++ agario-hack.js
@@ -0,0 +1,9 @@
+$("html").prepend("" +
+"<div id=\"agario-hack-overlay\" style=\"position:absolute;left:10;top:10;z-index:300;\">" +
+	"<div class=\"panel panel-default\">" +
+		"<div class=\"panel-body\">" +
+			"Test if it shows up :)" +
+		"</div>" +
+	"<div>" +
+"</div>"
+);
