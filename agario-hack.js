dd8622de7b9caa29f82a731d560e3b37346438d5
D3strukt0rLP <d3strukt0rlp@hotmail.com>
2015-06-30 16:42:00 +0200

    Update agario-hack.js

--- agario-hack.js
+++ agario-hack.js
@@ -5,7 +5,8 @@ var Config = {
 		"nonames" : "No names",
 		"darktheme" : "Dark Theme",
 		"nocolors" : "No colors",
-		"showmass" : "Show mass"
+		"showmass" : "Show mass",
+		"acid" : "Acid (Blur)"
 	}
 };
 
@@ -18,11 +19,12 @@ var Config = {
 	
 	function addOptions(element) {
 		element.html("");
-		element.append("<p><input type=\"checkbox\" onchange=\"setSkins(!$(this).is(':checked'));\"> " + Config.text.noskins + "</p>");
-		element.append("<p><input type=\"checkbox\" onchange=\"setNames(!$(this).is(':checked'));\"> " + Config.text.nonames + "</p>");
-		element.append("<p><input type=\"checkbox\" onchange=\"setDarkTheme($(this).is(':checked'));\"> " + Config.text.darktheme + "</p>");
-		element.append("<p><input type=\"checkbox\" onchange=\"setColors($(this).is(':checked'));\"> " + Config.text.nocolors + "</p>");
-		element.append("<p><input type=\"checkbox\" onchange=\"setShowMass($(this).is(':checked'));\"> " + Config.text.showmass + "</p>");
+		element.append("<p><label><input type=\"checkbox\" onchange=\"setSkins(!$(this).is(':checked'));\"> " + Config.text.noskins + "</label></p>");
+		element.append("<p><label><input type=\"checkbox\" onchange=\"setNames(!$(this).is(':checked'));\"> " + Config.text.nonames + "</label></p>");
+		element.append("<p><label><input type=\"checkbox\" onchange=\"setDarkTheme($(this).is(':checked'));\"> " + Config.text.darktheme + "</label></p>");
+		element.append("<p><label><input type=\"checkbox\" onchange=\"setColors($(this).is(':checked'));\"> " + Config.text.nocolors + "</label></p>");
+		element.append("<p><label><input type=\"checkbox\" onchange=\"setShowMass($(this).is(':checked'));\"> " + Config.text.showmass + "</label></p>");
+		element.append("<p><label><input type=\"checkbox\" onchange=\"setAcid($(this).is(':checked'));\"> " + Config.text.acid + "</label></p>");
 	}
 	
 	init();
